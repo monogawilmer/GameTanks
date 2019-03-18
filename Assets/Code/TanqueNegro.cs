332df864ed1eada91258bebaaf4bd79e8f0dec18
monogawilmer <monogawilmer@gmail.com>
2019-03-18 12:51:10 -0500

    MovimientoSubido
    Mueve Tanques

---
+++ Assets/Code/TanqueNegro.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TanqueNegro : MonoBehaviour
+{
+    [Range(1f,10f)]
+    public float velocidad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input.GetButton("Fire1")
+        // Input.GetAxis("Horizontal");
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //transform.Translate(-0.05f , 0, 0);
+            transform.Translate(Vector2.left * velocidad * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * velocidad * Time.deltaTime);
+        }
+    }
+}
